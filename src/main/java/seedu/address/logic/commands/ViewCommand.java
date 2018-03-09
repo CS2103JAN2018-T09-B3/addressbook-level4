48b6d28a7dec92ad953fe6f12bd0b440a054cd8c
iNekox3 <inekox3@hotmail.com>
2018-03-10 02:19:51 +0800

    ViewCommand: add message constant values

--- src/main/java/seedu/address/logic/commands/ViewCommand.java
+++ src/main/java/seedu/address/logic/commands/ViewCommand.java
@@ -5,5 +5,13 @@ package seedu.address.logic.commands;
  */
 public class ViewCommand extends Command {
 
+    public static final String COMMAND_WORD = "view";
 
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            // TODO: change description and parameter range when appropriate
+            + ": Toggle view to display outcomes in the specified week.\n"
+            + "Parameters: INDEX (must be a positive integer ranging from 1 to 13)\n"
+            + "Example: " + COMMAND_WORD + " 1";
+
+    public static final String MESSAGE_SUCCESS = "Viewing week %1$s";
 }
