bb35ff7ca2f507eb2bd76150338bb5e088f584cd
iNekox3 <inekox3@hotmail.com>
2018-03-10 02:48:27 +0800

    Add ViewCommandParser class

---
+++ src/main/java/seedu/address/logic/parser/ViewCommandParser.java
@@ -0,0 +1,10 @@
+package seedu.address.logic.parser;
+
+import seedu.address.logic.commands.ViewCommand;
+
+/**
+ * Parses input arguments and creates a new ViewCommand object
+ */
+public class ViewCommandParser implements Parser<ViewCommand> {
+
+}
