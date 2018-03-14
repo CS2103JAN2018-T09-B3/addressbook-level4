c50431de90c65b1ee593b455e38aa332f7091b7c
iNekox3 <inekox3@hotmail.com>
2018-03-15 00:52:56 +0800

    AddressBookParser: remove trailing whitespaces

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -96,7 +96,7 @@ public class AddressBookParser {
         case RedoCommand.COMMAND_WORD:
         case RedoCommand.COMMAND_ALIAS:
             return new RedoCommand();
-            
+
         case SortCommand.COMMAND_WORD:
             return new SortCommand();
 
