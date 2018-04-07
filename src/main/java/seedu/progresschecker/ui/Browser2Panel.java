340df683459f2299befeae8638ac77a1a43682b6
EdwardKSG <edwardkang97@gmail.com>
2018-04-07 18:11:32 +0800

    fix some failed tests

--- src/main/java/seedu/progresschecker/ui/Browser2Panel.java
+++ src/main/java/seedu/progresschecker/ui/Browser2Panel.java
@@ -1,5 +1,6 @@
 package seedu.progresschecker.ui;
 
+import java.net.URL;
 import java.util.logging.Logger;
 
 import com.google.common.eventbus.Subscribe;
@@ -9,6 +10,7 @@ import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.layout.Region;
 import javafx.scene.web.WebView;
+import seedu.progresschecker.MainApp;
 import seedu.progresschecker.commons.core.LogsCenter;
 import seedu.progresschecker.commons.events.ui.LoadBarEvent;
 
@@ -16,6 +18,8 @@ import seedu.progresschecker.commons.events.ui.LoadBarEvent;
  * The second Browser Panel of the App.
  */
 public class Browser2Panel extends UiPart<Region> {
+
+    public static final String DEFAULT_PAGE = "default.html";
 
     private static final String FXML = "BrowserPanel.fxml";
 
@@ -29,7 +33,21 @@ public class Browser2Panel extends UiPart<Region> {
 
         // To prevent triggering events for typing inside the loaded Web page.
         getRoot().setOnKeyPressed(Event::consume);
+
+        loadDefaultPage();
         registerAsAnEventHandler(this);
+    }
+
+    public void loadPage(String url) {
+        Platform.runLater(() -> browser.getEngine().load(url));
+    }
+
+    /**
+     * Loads a default HTML file with a background that matches the general theme.
+     */
+    private void loadDefaultPage() {
+        URL defaultPage = MainApp.class.getResource(FXML_FILE_FOLDER + DEFAULT_PAGE);
+        loadPage(defaultPage.toExternalForm());
     }
 
     //@@author EdwardKSG
