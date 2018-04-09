b5d247cf492293e9de0e7eb947558abf826ec9c1
iNekox3 <inekox3@hotmail.com>
2018-04-10 04:26:04 +0800

    ViewCommand: modify to execute results differently based on type of user input

--- src/main/java/seedu/progresschecker/logic/commands/ViewCommand.java
+++ src/main/java/seedu/progresschecker/logic/commands/ViewCommand.java
@@ -16,26 +16,45 @@ public class ViewCommand extends Command {
     public static final String MESSAGE_USAGE = COMMAND_WORD
             + ": Change the tab view to profiles, tasks or exercises.\n"
             + "Parameters: TYPE (must be either 'profile', 'task', or 'exercise')\n"
-            + "Example: " + COMMAND_WORD + " exercise";
+            + "INDEX (if TYPE is exercise and must be within 2 and 12 (boundary numbers inclusive)\n"
+            + "Example: " + COMMAND_WORD + " exercise\n"
+            + COMMAND_WORD + "exercise 5";
 
-    public static final String MESSAGE_SUCCESS = "Viewing tab %1$s";
+    public static final String MESSAGE_SUCCESS_TAB = "Viewing tab %1$s";
+    public static final String MESSAGE_SUCCESS_WEEK = "Viewing week %1$s's exercises";
+
+    public static final int MIN_WEEK_NUMBER = 2;
+    public static final int MAX_WEEK_NUMBER = 12;
 
     private final String type;
+    private final int weekNumber;
+    private final boolean isWeekToggle;
 
-    public ViewCommand(String type) {
+    public ViewCommand(String type, int weekNumber, boolean isWeekToggle) {
         this.type = type;
+        this.weekNumber = weekNumber;
+        this.isWeekToggle = isWeekToggle;
     }
 
     @Override
     public CommandResult execute() {
-        EventsCenter.getInstance().post(new TabLoadChangedEvent(type));
-        return new CommandResult(String.format(MESSAGE_SUCCESS, type));
+        if (!isWeekToggle) {
+            EventsCenter.getInstance().post(new TabLoadChangedEvent(type));
+            return new CommandResult(String.format(MESSAGE_SUCCESS_TAB, type));
+        }
+        else {
+            model.updateFilteredExerciseList(exercise -> exercise.getQuestionIndex().getWeekNumber() == weekNumber);
+            EventsCenter.getInstance().post(new TabLoadChangedEvent(type));
+            return new CommandResult(String.format(MESSAGE_SUCCESS_WEEK, weekNumber));
+        }
     }
 
     @Override
     public boolean equals(Object other) {
         return other == this // short circuit if same object
                 || (other instanceof ViewCommand // instanceof handles nulls
-                && this.type.equals(((ViewCommand) other).type)); // state check
+                && this.type.equals(((ViewCommand) other).type)) // state check
+                && this.weekNumber == ((ViewCommand) other).weekNumber
+                && (this.isWeekToggle && ((ViewCommand) other).isWeekToggle);
     }
 }
