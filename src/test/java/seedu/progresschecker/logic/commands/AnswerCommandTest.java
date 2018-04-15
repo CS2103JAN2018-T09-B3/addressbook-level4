3d3fb983b20e754bf35722140f060d315a517085
iNekox3 <inekox3@hotmail.com>
2018-04-15 20:16:24 +0800

    Add view and answer command tests

--- src/test/java/seedu/progresschecker/logic/commands/AnswerCommandTest.java
+++ src/test/java/seedu/progresschecker/logic/commands/AnswerCommandTest.java
@@ -1,39 +1,48 @@
 package seedu.progresschecker.logic.commands;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static seedu.progresschecker.testutil.TypicalPersons.getTypicalProgressChecker;
+
+import org.junit.Rule;
 import org.junit.Test;
-import seedu.progresschecker.logic.commands.exceptions.CommandException;
-import seedu.progresschecker.model.ProgressChecker;
-import seedu.progresschecker.model.ReadOnlyProgressChecker;
+import org.junit.rules.ExpectedException;
+
 import seedu.progresschecker.model.exercise.Exercise;
-import seedu.progresschecker.model.exercise.exceptions.DuplicateExerciseException;
-import seedu.progresschecker.model.person.Person;
-import seedu.progresschecker.model.person.exceptions.DuplicatePersonException;
-import seedu.progresschecker.testutil.PersonBuilder;
+import seedu.progresschecker.model.exercise.StudentAnswer;
+import seedu.progresschecker.testutil.ExerciseBuilder;
 
+//@@author iNekox3
 public class AnswerCommandTest {
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
     @Test
-    public void execute_duplicatePerson_throwsCommandException() throws Exception {
-        AnswerCommandTest.ModelStub modelStub = new AnswerCommandTest.ModelStubThrowingDuplicateExerciseException();
-        Person validPerson = new PersonBuilder().build();
-
-        thrown.expect(CommandException.class);
-        thrown.expectMessage(AddCommand.MESSAGE_DUPLICATE_PERSON);
-
-        getAddCommandForExecise(validPerson, modelStub).execute();
+    public void constructor_nullExercise_throwsNullPointerException() {
+        thrown.expect(NullPointerException.class);
+        new AnswerCommand(null, null);
     }
 
-    /**
-     * A Model stub that always throw a DuplicateExerciseException when trying to add a exercise.
-     */
-    private class ModelStubThrowingDuplicateExerciseException extends AnswerCommandTest.ModelStub {
-        @Override
-        public void addExercise(Exercise exercise) throws DuplicateExerciseException {
-            throw new DuplicateExerciseException();
-        }
+    @Test
+    public void equals() {
+        Exercise exercise1 = new ExerciseBuilder().withStudentAnswer("a b c").build();
+        Exercise exercise2 = new ExerciseBuilder().withStudentAnswer("d e f").build();
+        AnswerCommand answerExercise1Command = new AnswerCommand(exercise1.getQuestionIndex(),
+                new StudentAnswer("a b c"));
+        AnswerCommand answerExercise2Command = new AnswerCommand(exercise2.getQuestionIndex(),
+                new StudentAnswer("d e f"));
 
-        @Override
-        public ReadOnlyProgressChecker getProgressChecker() {
-            return new ProgressChecker();
-        }
+        // same object -> returns true
+        assertTrue(answerExercise1Command.equals(answerExercise1Command));
+
+        // different types -> returns false
+        assertFalse(answerExercise1Command.equals(1));
+
+        // null -> returns false
+        assertFalse(answerExercise1Command.equals(null));
+
+        // different person -> returns false
+        assertFalse(answerExercise1Command.equals(answerExercise2Command));
     }
 }
